469b96f7d6356e76d7a3f19d5e08981788d80d0c
Brandon Huang <bhuang111@yahoo.com>
2016-10-02 22:59:30 -0700

    add methods take_word, limit_reached, words_guessed and driver code

--- ruby/games/guessing_game.rb
+++ ruby/games/guessing_game.rb
@@ -4,8 +4,31 @@ class Word_game
 
   def initialize(word_length)
     @list = []
+    @guessed_letters = []
+    @count = 0
     word_length.times {@list << "_"}
 
+  end
+
+  def take_word(word)
+    word = word.split("")
+  end
+
+  def limit_reached(guesses,limit)
+    if gusses == limit
+      true
+    else
+      false
+    end
+  end
+
+  def words_guessed(letter)
+    if @guessed_letters.include? letter
+      return @count
+    else
+      @guessed_letters << letter
+      @count += 1
+    end
   end
 
   def print
@@ -16,3 +39,7 @@ end
 
 word = Word_game.new(5)
 word.print
+guess = word.take_word("string")
+word.words_guessed("s")
+p word.words_guessed("s")
+p word.words_guessed("a")

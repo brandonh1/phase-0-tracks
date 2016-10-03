9a7522e4e180b6722edabd683e602e5e95b9b97a
Brandon Huang <bhuang111@yahoo.com>
2016-10-02 23:18:21 -0700

    add method to check whether guess is correct or not, plus driver code

--- ruby/games/guessing_game.rb
+++ ruby/games/guessing_game.rb
@@ -22,7 +22,7 @@ class Word_game
     end
   end
 
-  def words_guessed(letter)
+  def letters_guessed(letter)
     if @guessed_letters.include? letter
       return @count
     else
@@ -31,15 +31,33 @@ class Word_game
     end
   end
 
+  def right_or_wrong(letter,guess)
+    index = 0
+    if !guess.include? letter
+      print
+    else
+      guess.each do |i|
+        if letter == i
+          @list[index] = letter
+        end
+        index += 1
+      end
+    end
+  end
+
+
   def print
      puts guessed = @list.join(" ")
   end
 
 end
 
-word = Word_game.new(5)
+word = Word_game.new(6)
 word.print
-guess = word.take_word("string")
-word.words_guessed("s")
-p word.words_guessed("s")
-p word.words_guessed("a")
+guess = word.take_word("strirg")
+word.right_or_wrong("r",guess)
+word.print
+word.right_or_wrong("s",guess)
+word.print
+
+

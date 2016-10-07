b32c6ffc64498ae29d7715944260a6656db285e2
Brandon Huang <bhuang111@yahoo.com>
2016-10-07 02:07:20 -0700

    make function and add driver code

--- js/explore.js
+++ js/explore.js
@@ -5,3 +5,19 @@ use for loop to loop backwards
 store each letter into the new string
 return new string
 */
+
+function reverse(str) {
+	var newStr = ""
+	var splitStr = str.split("")
+	for(i = str.length-1; i >= 0; i--) {
+		newStr += str[i];
+	}
+	return newStr
+}
+
+var reversed = reverse("hello")
+if(true) {
+	console.log(reversed);
+} else {
+	console.log("not reversed");
+}

251341aed35d999e04011c3f5878d4cb1987fc0a
Brandon Huang <bhuang111@yahoo.com>
2016-10-17 00:29:31 -0700

    make methods to update emails and remove person off list

--- databases/8.5/signups.rb
+++ databases/8.5/signups.rb
@@ -11,8 +11,24 @@ create_table_cmd = <<-SQL
   )
 SQL
 
+update_email_cmd = <<-SQL
+  UPDATE signups SET email = ? WHERE id = ?
+SQL
+
+delete_row_cmd = <<-SQL
+  DELETE FROM signups WHERE id = ?
+SQL
+
 def add_signup(list,name,email)
   list.execute("INSERT INTO signups (name,email) values (?,?)", [name,email])
+end
+
+def update_email(list,update_email_cmd,email,id)
+  list.execute(update_email_cmd,[email,id])
+end
+
+def delete_row(list,delete_row_cmd,id)
+  list.execute(delete_row_cmd,id)
 end
 
 def show_list(list)
@@ -25,4 +41,5 @@ end
 list.execute(create_table_cmd)
 
 add_signup(list,"person","email@email.com")
+update_email(list,update_email_cmd,"yahoo@yahoo.com",2)
 show_list(list)

4553d907b1a9d76188cd6c9f3e281d60e5229c82
Brandon Huang <bhuang111@yahoo.com>
2016-10-21 18:58:43 -0700

    add function to add another lizard

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -8,8 +8,27 @@ function showPhoto() {
   photo.hidden = false;
 }
 
+function addLizard() {
+ 
+  var img=document.createElement("img");
+  img.src="chameleon.png"
+  img.id="picture"
+  img.setAttribute('height', 'auto');
+  img.setAttribute('width', '225px')
+  document.body.appendChild(img);
+  
+}
+
+var newBtn = document.createElement("button");
+var text = document.createTextNode("Add another lizard")
+newBtn.appendChild(text);
+document.getElementById("about-my-pet").appendChild(newBtn);
+newBtn.id = "lizard-btn";
 
 var hide = document.getElementById("hide");
 hide.addEventListener("click",hidePhoto);
 var show = document.getElementById("show");
 show.addEventListener("click",showPhoto);
+
+var add_button = document.getElementById("lizard-btn");
+add_button.addEventListener("click", addLizard);
